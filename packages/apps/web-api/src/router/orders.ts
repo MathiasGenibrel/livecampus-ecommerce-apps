79fa7c880ecc8fc5ac926687aa6e40fb73e0be38
MathiasGenibrel <mathias.geni@gmail.com>
2023-02-28 23:17:34 +0100

    💡 Add TODO

--- packages/apps/web-api/src/router/orders.ts
+++ packages/apps/web-api/src/router/orders.ts
@@ -6,10 +6,13 @@ const orders = new OrdersController();
 export const ordersRouter = (app: Express) => {
   const router = Router({ caseSensitive: false });
 
+  // TODO: Add validation pipe middleware (to control params)
   router.get('/history', orders.history);
 
+  // TODO: Add validation pipe middleware (to control input)
   router.post('/', orders.create);
 
+  // TODO: Add auth && validation pipe middleware
   router.put('/updateStatus', orders.updateStatus);
 
   app.use('/orders', router);
--- packages/apps/web-api/src/router/users.ts
+++ packages/apps/web-api/src/router/users.ts
@@ -7,15 +7,19 @@ export const usersRouter = (app: Express) => {
   const router = Router({ caseSensitive: false });
 
   // Get user credential
-  router.get('/login', users.login);
+  // TODO: Add validation pipe middleware (to control input)
+  router.post('/login', users.login);
 
   // Register an new user in database
+  // TODO: Add validation pipe middleware (to control input)
   router.post('/register', users.register);
 
   // Modify user data
+  // TODO: Add validation pipe middleware (to control input)
   router.put('/edit', users.edit);
 
   // Remove an user from database
+  // TODO: Add validation pipe middleware (to control params)
   router.delete('/delete', users.delete);
 
   app.use('/users', router);
