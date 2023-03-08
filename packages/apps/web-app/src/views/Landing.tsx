f71a0df82ae30518c31e6e8642f41ca0628a2413
MathiasGenibrel <mathias.geni@gmail.com>
2023-03-08 15:33:02 +0100

    ♻️ Add Header to Landing component

--- packages/apps/web-app/src/views/Landing.tsx
+++ packages/apps/web-app/src/views/Landing.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import { useQuery } from 'react-query';
 import { NavigateFunction, useNavigate } from 'react-router-dom';
 
-import { Card } from '../components/Card/Card';
-import { useQuery } from 'react-query';
 import { LocalRepository } from '../repository/products/local-repository';
+
+import { Card } from '../components/Card/Card';
+import { Headers } from '../components/Header/Headers';
 import { CardSkeleton } from '../components/Card/CardSkeleton';
 import { Hero } from '../components/Hero/Hero';
 
@@ -17,23 +19,27 @@ export const Landing = () => {
   );
 
   return (
-    <main>
-      <Hero />
+    <>
+      <Headers />
 
-      <article className={'mx-4 my-8'}>
-        <h2 className={'text-xl'}>Headphones For You!</h2>
-        <ul className={'flex flex-wrap my-4 gap-4'}>
-          {isLoading &&
-            // Array of skeleton card
-            Array.from(Array(6).keys()).map((index) => (
-              <CardSkeleton key={index} />
-            ))}
-          {data &&
-            data.map((product) => (
-              <Card key={product.id} product={product} navigate={navigate} />
-            ))}
-        </ul>
-      </article>
-    </main>
+      <main>
+        <Hero />
+
+        <article className={'mx-4 my-8'}>
+          <h2 className={'text-xl'}>Headphones For You!</h2>
+          <ul className={'flex flex-wrap my-4 gap-4'}>
+            {isLoading &&
+              // Array of skeleton card
+              Array.from(Array(6).keys()).map((index) => (
+                <CardSkeleton key={index} />
+              ))}
+            {data &&
+              data.map((product) => (
+                <Card key={product.id} product={product} navigate={navigate} />
+              ))}
+          </ul>
+        </article>
+      </main>
+    </>
   );
 };
