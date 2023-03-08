575beaf8d39bde0eb432a58db24688481d3e1fd4
MathiasGenibrel <mathias.geni@gmail.com>
2023-03-08 15:30:23 +0100

    ♻️Use new methods of react router
    Use createBrowserRouter instead of BrowserRouter.

--- packages/apps/web-app/src/components/App/App.tsx
+++ packages/apps/web-app/src/components/App/App.tsx
@@ -1,15 +1,16 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
 import { AppRouter } from '../../router/AppRouter';
-import * as process from 'process';
+import { RouterProvider } from 'react-router-dom';
 
 export const App = () => {
   const queryClient = new QueryClient();
 
   return (
     <QueryClientProvider client={queryClient}>
+      {/* Browser router */}
+      <RouterProvider router={AppRouter} />
       <ReactQueryDevtools />
-      <AppRouter />
     </QueryClientProvider>
   );
 };
--- packages/apps/web-app/src/router/AppRouter.tsx
+++ packages/apps/web-app/src/router/AppRouter.tsx
@@ -1,18 +1,23 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import { Landing } from '../views/Landing';
-import { NotFound } from '../views/errors/NotFound';
-import { Headers } from '../components/Header/Headers';
+import { NotFoundError } from '../views/errors/NotFoundError';
+import { Product } from '../views/Product';
+import { InternalServerError } from '../views/errors/InternalServerError';
 
-export const AppRouter = () => {
-  return (
-    <>
-      <Headers />
-      <Routes>
-        <Route path={'/'} element={<Landing />} />
-        <Route path={'/*'} element={<NotFound />} />
-      </Routes>
-    </>
-  );
-};
+export const AppRouter = createBrowserRouter([
+  {
+    path: '/',
+    element: <Landing />,
+  },
+  {
+    path: '/product/:id',
+    element: <Product />,
+    errorElement: <InternalServerError />,
+  },
+  {
+    path: '/*',
+    element: <NotFoundError />,
+  },
+]);
